09347c5308fe26359d344ea0790e448a23d3c1c4
Ahyana A <aatibada@gmail.com>
2023-08-29 11:22:47 -0400

    Add HTML, Nav functionality, and card styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+// Import UseState
+import { useState } from "react";
+
+// Import data
+import data from "./data.json";
+
+// Import images
+import profileImg from "./images/image-jeremy.png";
+import ellipsisIcon from "./images/icon-ellipsis.svg";
 
 function App() {
+  const [selectedTimeframe, setSelectedTimeframe] = useState("weekly");
+
+  function handleClick(event) {
+    const selected = event.target.innerHTML;
+    switch (selected) {
+      case "Daily":
+        setSelectedTimeframe("daily");
+        break;
+      case "Weekly": 
+        setSelectedTimeframe("weekly");
+        break;
+      case "Monthly":
+        setSelectedTimeframe("monthly");
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <section className="profile-card card">
+          <div className="profile">
+            <img src={profileImg} alt="Picture of Jeremy" />
+            <h1>Report for Jeremy Robson</h1>
+          </div>
+          <nav>
+            <ul>
+              <li onClick={handleClick}>Daily</li>
+              <li onClick={handleClick}>Weekly</li>
+              <li onClick={handleClick}>Monthly</li>
+            </ul>
+          </nav>
+        </section>
+        <section className="cards">
+          {data.map((category) => {
+            switch (selectedTimeframe) {
+              case "daily":
+                return (
+                  <Card
+                    key={category.title}
+                    title={category.title}
+                    timeframe={category.timeframes.daily}
+                  />
+                );
+              case "weekly":
+                return (
+                  <Card
+                    key={category.title}
+                    title={category.title}
+                    timeframe={category.timeframes.weekly}
+                  />
+                );
+              case "monthly":
+                return (
+                  <Card
+                    key={category.title}
+                    title={category.title}
+                    timeframe={category.timeframes.monthly}
+                  />
+                );
+            }
+          })}
+        </section>
+      </main>
     </div>
   );
-}
+};
+
+function Card(props) {
+
+  return (
+    <section className="task-folder">
+      <span className={props.title}>
+        {/* <img src={workIcon} alt="Suitcase icon" /> */}
+      </span>
+      <div className="card">
+        <div className="header">
+          <h2>{props.title}</h2>
+          <img src={ellipsisIcon} alt="Ellipsis icon" />
+        </div>
+        <div className="time">
+          <p className="current">{props.timeframe.current}hrs</p>
+          <p className="previous">{props.timeframe.previous}hrs</p>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default App;
